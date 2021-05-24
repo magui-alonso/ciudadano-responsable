1f8e5eaf86c0fbedd3491064454a9e8eaadcd3c7
magui-alonso <magui.alonso.g@gmail.com>
2021-05-24 16:26:24 -0300

    cambios en php

--- envia.php
+++ envia.php
@@ -1,6 +1,6 @@
 ﻿<?php
 $remitente = $_POST['email'];
-$destinatario = 'info@caromontalto.com'; // en esta línea va el mail del destinatario.
+$destinatario = 'ciudadanoresponsable.contacto@gmail.com'; // en esta línea va el mail del destinatario.
 $asunto = 'Contacto Ciudadano Responsable'; // acá se puede modificar el asunto del mail
 if (!$_POST){
 ?>
